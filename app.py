3dd1fc6b53bb88476c711bc0f82c48262d19c043
Jatim AI <jatim.ai.tech@gmail.com>
2020-12-24 16:34:42 +0700

    modifying date publish

---
+++ app.py
@@ -0,0 +1,3 @@
+from newsplease import NewsPlease as nwpl
+
+article = nwpl.from_url('https://www.cnbcindonesia.com/tech/20201224100526-37-211378/mohon-maaf-ini-alasan-vaksin-covid-19-tak-bisa-akhiri-pandem')
